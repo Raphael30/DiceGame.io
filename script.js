a591d5530e33400cee31c74dac262a20ddcac735
Raphael30 <91823980+Raphael30@users.noreply.github.com>
2021-10-22 22:31:49 +0800

    Upload project DiceGame

---
+++ script.js
@@ -0,0 +1,94 @@
+'use strict';
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+
+const current0 = document.querySelector('#current--0');
+const current1 = document.querySelector('#current--1');
+const dice = document.querySelector('.dice');
+
+const diceEl = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const announecer = document.querySelector('.instruction');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+
+  dice.classList.add('hidden');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+
+  announecer.textContent = 'First to 50!';
+  announecer.classList.remove('winner');
+};
+init();
+
+// ********* SWITCH PLAYER *********
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
+
+// ********* ROLL DICE *********
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// ********* BTN HOLD *********
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 50) {
+      playing = false;
+      dice.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        'WINNER';
+      announecer.textContent = '🎉 WE HAVE A WINNER 🎉';
+      announecer.classList.add('winner');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// ********* RESET GAME *********
+btnNewGame.addEventListener('click', init);
